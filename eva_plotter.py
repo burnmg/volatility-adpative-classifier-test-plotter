6a4ff31e5ab095c72d25ffc4df75b9cee6e46768
burnmg <rlin1992@hotmail.com>
2016-05-09 16:55:12 +1200

    c

--- eva_plotter.py
+++ eva_plotter.py
@@ -9,24 +9,27 @@ def main(args):
     x = []
     correct_rate = []
     vol_drift_positions = []
+    plt.ylim([50, 100])
+
     with open(args[0], 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
             x.append(float(row['learning evaluation instances']))
             correct_rate.append(float(row['classifications correct (percent)']))
+    plt.plot(x, correct_rate, 'r')
 
     with open(args[1], 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
             vol_drift_positions.append(float(row['VolatilityDriftInstance']))
 
-    plt.plot(x, correct_rate, 'r')
-    plt.axvline(vol_drift_positions, color='b', linestyle="---")
+
+    plt.axvline(vol_drift_positions, color='b')
 
     plt.xlabel('instance')
     plt.ylabel('percentage of correct')
     plt.grid(True)
-
+    plt.show()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
