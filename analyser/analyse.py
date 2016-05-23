bf4a3512b0b7ec1ce691683f725244d33ac969a3
burnmg <rlin1992@hotmail.com>
2016-05-23 18:59:02 +1200

    c

--- analyser/analyse.py
+++ analyser/analyse.py
@@ -2,54 +2,64 @@ import csv
 import matplotlib.pyplot as plt
 import os.path
 import numpy as np
+import sys
 
-instances_index = []
-correct_rates = []
-bytes = []
-times = []
 
-# correct rate plot
-res_file = "res.csv"
-if os.path.isfile(res_file):
+def analyse(res_file, drift_point_file, to_summary_file, to_figure_file):
+    instances_index = []
+    correct_rates = []
+    bytes = []
+    times = []
 
-    with open(res_file, 'rb') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            instances_index.append(float(row['learning evaluation instances']))
-            correct_rates.append(float(row['classifications correct (percent)']))
-            bytes.append(float(row['model serialized size (bytes)']))
-            times.append(float(row['evaluation time (cpu seconds)']))
-    plt.plot(instances_index, correct_rates, 'r')
+    # correct rate plot
+    if os.path.isfile(res_file):
 
-    # write avg correct_rates, time, byte
-    mean_correct_rates = np.mean(correct_rates)
-    mean_bytes = np.mean(bytes)
+        with open(res_file, 'rb') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                instances_index.append(float(row['learning evaluation instances']))
+                correct_rates.append(float(row['classifications correct (percent)']))
+                bytes.append(float(row['model serialized size (bytes)']))
+                times.append(float(row['evaluation time (cpu seconds)']))
+        plt.plot(instances_index, correct_rates, 'r')
 
-    with open('summary.csv', 'w') as summary:
-        fieldnames = ['mean correct rate', 'time', 'mean byte']
-        writer = csv.DictWriter(summary, fieldnames=fieldnames)
+        # write avg correct_rates, time, byte
+        mean_correct_rates = np.mean(correct_rates)
+        mean_bytes = np.mean(bytes)
 
-        writer.writeheader()
-        writer.writerow({
-            'mean correct rate': mean_correct_rates,
-            'time': times[len(times)-1],
-            'mean byte': mean_bytes
+        with open(to_summary_file, 'w') as summary:
+            fieldnames = ['mean correct rate', 'time', 'mean byte']
+            writer = csv.DictWriter(summary, fieldnames=fieldnames)
 
-        })
+            writer.writeheader()
+            writer.writerow({
+                'mean correct rate': mean_correct_rates,
+                'time': times[len(times)-1],
+                'mean byte': mean_bytes
 
-plt.xlabel('instance')
-plt.ylabel('percentage of correct')
+            })
 
-# change point plot
-change_point_path = "changepoint.csv"
-if os.path.isfile(change_point_path):
-    change_points = []
-    with open(change_point_path, 'r') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            change_points.append(float(row['ClassifierChangePoint']))
-    for i in change_points:
-        plt.axvline(i, color='b')
+    plt.xlabel('instance')
+    plt.ylabel('percentage of correct')
 
-plt.grid(True)
-plt.show()
+    # change point plot
+    if os.path.isfile(drift_point_file):
+        change_points = []
+        with open(drift_point_file, 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                change_points.append(float(row['ClassifierChangePoint']))
+        for i in change_points:
+            plt.axvline(i, color='b')
+
+    plt.grid(True)
+    plt.savefig(to_figure_file)
+
+
+def main(argv):
+    analyse(argv[0], argv[1], argv[2], argv[3])
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
